3a6b285bb5ecbb0d221d9f68231b7bfcbed6d967
Lais Jacqueline Silva <laisjssp@gmail.com>
2019-05-24 17:38:58 -0300

    Adicionando estrutura condicional ao sistema

--- index.php
+++ index.php
@@ -1,44 +1,140 @@
+<?php
+
+$usuario = ["logado"=> false, "nome"=> "Lais J Silva", "nivelAcesso"=> 0];
+//$usuario = "";
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>  
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Loja Virtual</title>
+        </head>
+        <!-- bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <?php
+        <header>
 
-       $nome = "Lais";
+        <!--aqui ta a nav-->
 
-// tudo que tiver com cifrão e uma palavra é uma variável (toda vez que colocar o cifrão transforma em variável)
-       
-// tem que ter sempre o $ pq sem isso ele entende que é uma constante, aquela que nunca muda de valor ou seja pra imprimir
-//o nome você coloca o cifrão e o nome da variável que é "nome"
 
-if ($nome == "Lais") {
-    var_dump ($nome. "<br>");
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!--o que chamamos de conteiener-->
 
-    //o var_dump imprimi a variavel, fala que tipo que ela é e o tamanho da string (ele soma tudo que estiver dentro do parenteses como string)
-      //quando coloca aspas duplas ele aceita a variável dentro, agora se colocar aspas simples ele imprime o texto
-    //literal
-    }  
-    else {
-        echo "Não é a Laís";
-    }
-//no parenteses vai a condição e na chaves a resposta se for verdadeira
-// pra imprimir um if fica o echo e a variável
+        <a class="navbar-brand" href="#">Cursos</a>
 
-for ($i = 0; $i < 5; $i++){
-    echo "Olá $nome" . "<br>";
-    // é com o ponto que fazemos concatenação de informações
-    //quando coloca aspas duplas ele aceita a variável dentro, agora se colocar aspas simples ele imprime o texto
-    //literal
-    }
+        <!--aqui fica o nome do projeto-->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
 
-    ?>
+        <!--ai fica eh uma div que segura a ul que quando estiver nor esponsivo transforma em menu hamburger -->
+            <ul class="navbar-nav">
 
+            <?php  if(isset ($usuario) && $usuario != "" && $usuario ['logado']): ?>
+            <!--isset = ess variavel existe?-->
+
+                    <?php if ($usuario ['nivelAcesso'] == 0): ?>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Ações <span class="sr-only">(current)</span></a>
+                </li>
+
+                    <?php else: ?>
+
+    <!--else da variavel usuario existente?-->
+
+                
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Perfil <span class="sr-only">(current)</span></a>
+                </li>
+
+                    <?php endif; ?>           
+
+                <li class= "nav-item">
+                <a class= "nav-link" href= "#"> Olá <?php echo $usuario ['nome'];?></a>
+
+                </li>
+
+                   <?php else: ?>
+
+                
+                  <li class= "nav-item">
+                <a class= "nav-link" href= "#"> Login </a>
+
+                </li>
+                 <?php endif;?>
+
+
+                </ul>
+            </div>
+            </nav>
+
+            </header>
+
+            <main class= "container mt-5">
+
+            <!-- ela vai segurar os cards. Tag main, ela fala que começa todo o conteúdo, a main substitui o section-->
+              <section class= "row">
+
+              <!--coluna para segurar-->
+
+              <div class= "col-md-4"> 
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+
+
+            <div class= "col-md-4"> 
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            </div>
+
+            <div class= "col-md-4"> 
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+
+              </section>
+            </main>
+
+    <body> 
+
+    
+
+
+
+
+<!-- javascript -->
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
      
 </body>
 </html>
